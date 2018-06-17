59c1b4a51677c97e4a15fa4e0a7057ae62c1fb90
CyCraig <CyCraig@users.noreply.github.com>
2018-06-17 19:11:01 +0200

    Finished initial porting to OpenAI Gym.
    First working version.

--- setup.py
+++ setup.py
@@ -2,5 +2,7 @@ from setuptools import setup
 
 setup(name='gym_platform',
       version='0.0.1',
-      install_requires=['gym']  # And any other dependencies
+      install_requires=['gym',
+                        'pygame>=1.9.3'
+                        'numpy>=1.14.0']
 ) 
